22d5318cffc991237c879190d093a9c342f128a0
AnthonyKopri <62609903+AnthonyKopri@users.noreply.github.com>
2020-11-25 22:10:19 +0300

    Initial commit

---
+++ Assets/AutoMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    public float gravForce = 5f;
+    public bool gravSwitch = false;
+
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed;
+        if (Input.GetMouseButtonDown(0))
+        {
+            gravSwitch = !gravSwitch;
+        }
+        if (gravSwitch)
+        {
+            Physics.gravity = new Vector3(0, gravForce, 0);
+        }
+        else
+        {
+            Physics.gravity = new Vector3(0, -gravForce, 0);
+        }
+    }
+
+}
