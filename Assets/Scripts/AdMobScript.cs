c0f9cf4667cd0c072af728572663818d3cb7d14a
AnthonyKopri <62609903+AnthonyKopri@users.noreply.github.com>
2021-01-09 16:15:20 +0300

    Ad Stuff

---
+++ Assets/Scripts/AdMobScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using GoogleMobileAds.Api;
+
+public class AdMobScript : MonoBehaviour
+{
+
+    private RewardBasedVideoAd rewardBasedVideoAd;
+
+    #if UNITY_ANDROID
+        string APP_ID = "ca-app-pub-3940256099942544/5224354917";
+    #elif UNITY_IPHONE
+        string APP_ID = "ca-app-pub-3940256099942544/5224354917";
+    #else
+        string APP_ID = "unexpected_platform";
+    #endif
+
+
+    void Start()
+    {
+        Debug.Log(APP_ID);
+        rewardBasedVideoAd = RewardBasedVideoAd.Instance;
+        MobileAds.Initialize(APP_ID);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
